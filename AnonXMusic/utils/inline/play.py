90e38b745687743c3fbac342729295918a58bdca
MTSTUDIO84 <144623587+MTSTUDIO84@users.noreply.github.com>
2024-09-13 20:10:28 +0530

    Update play.py
    Signed-off-by: MTSTUDIO84 <144623587+MTSTUDIO84@users.noreply.github.com>

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -1,8 +1,8 @@
 import math
-
+from config import SUPPORT_CHAT, OWNER_USERNAME
 from pyrogram.types import InlineKeyboardButton
-
-from AnonXMusic.utils.formatters import time_to_seconds
+from DAXXMUSIC import app
+from DAXXMUSIC.utils.formatters import time_to_seconds
 
 
 def track_markup(_, videoid, user_id, channel, fplay):
@@ -33,25 +33,25 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "◉—————————"
+        bar = "🙏—————————"
     elif 10 < umm < 20:
-        bar = "—◉————————"
+        bar = "—💫————————"
     elif 20 <= umm < 30:
-        bar = "——◉———————"
+        bar = "——✨———————"
     elif 30 <= umm < 40:
-        bar = "———◉——————"
+        bar = "———💎——————"
     elif 40 <= umm < 50:
-        bar = "————◉—————"
+        bar = "————💖—————"
     elif 50 <= umm < 60:
-        bar = "—————◉————"
+        bar = "—————❤————"
     elif 60 <= umm < 70:
-        bar = "——————◉———"
+        bar = "——————🥰———"
     elif 70 <= umm < 80:
-        bar = "———————◉——"
+        bar = "———————🤗——"
     elif 80 <= umm < 95:
-        bar = "————————◉—"
+        bar = "————————😇—"
     else:
-        bar = "—————————◉"
+        bar = "—————————👋"
     buttons = [
         [
             InlineKeyboardButton(
@@ -60,31 +60,73 @@ def stream_markup_timer(_, chat_id, played, dur):
             )
         ],
         [
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],
+        [
             InlineKeyboardButton(
-                text="𝖴𝗉𝖽𝖺𝗍𝖾𝗌 📢", url=f"https://t.me/Dns_Official_Channel"
+                text="✪ʟɪꜰᴇ✪",
+                url=f"https://t.me/Team_riya_support",
             ),
             InlineKeyboardButton(
-                text="𝖲𝗎𝗉𝗉𝗈𝗋𝗍 💬", url=f"https://t.me/DNS_NETWORK"
-          ),
+                text="✪ᴊᴀɴɴ✪",
+                url=f"https://t.me/Ksd_bot_network",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="✪ᴏᴡɴᴇʀ✪",
+                url=f"t.me/{OWNER_USERNAME}",
+            ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
+        
 
 
 def stream_markup(_, chat_id):
     buttons = [
         [
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],
+                 [
             InlineKeyboardButton(
-                text="𝖴𝗉𝖽𝖺𝗍𝖾𝗌 📢", url=f"https://t.me/Dns_Official_Channel"
+
+                text="✪ʟɪꜰᴇ✪",
+
+                url=f"https://t.me/Team_riya_support",
+
             ),
             InlineKeyboardButton(
-                text="𝖲𝗎𝗉𝗉𝗈𝗋𝗍 💬", url=f"https://t.me/DNS_NETWORK"
-          ),
+
+                text="✪ᴊᴀɴɴ✪",
+
+                url=f"https://t.me/Ksd_bot_network",
+
+            ),
+        ],
+
+                 [
+            InlineKeyboardButton(
+
+                text="✪ᴏᴡɴᴇʀ✪",
+
+                url=f"t.me/{OWNER_USERNAME}",
+
+            ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
+    
 
 
 def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
@@ -92,11 +134,11 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
         [
             InlineKeyboardButton(
                 text=_["P_B_1"],
-                callback_data=f"AnonyPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
+                callback_data=f"DAXXPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
             ),
             InlineKeyboardButton(
                 text=_["P_B_2"],
-                callback_data=f"AnonyPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
+                callback_data=f"DAXXPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
             ),
         ],
         [
@@ -156,3 +198,4 @@ def slider_markup(_, videoid, user_id, query, query_type, channel, fplay):
         ],
     ]
     return buttons
+    
